909d25397066cf0266778a961caa8ac47553446c
caique <felipecaique48@gmail.com>
2026-02-23 11:36:26 -0300

    enable swagger

--- Cadastro.Pro.Api/Program.cs
+++ Cadastro.Pro.Api/Program.cs
@@ -42,12 +42,9 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
+
 
 // 🔥 PORTA DO RENDER (SÓ UMA VEZ E NO FINAL)
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
 app.Run($"http://0.0.0.0:{port}");
